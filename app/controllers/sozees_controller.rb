d92030c17fb27c59c92d26082353933d2aae222d
Joffrey Cazalbou <joffrey.cazalbou@gmail.com>
2017-11-27 18:12:13 +0100

    new and create methods for sozee on-going

---
+++ app/controllers/sozees_controller.rb
@@ -0,0 +1,21 @@
+class SozeesController < ApplicationController
+
+  def new
+    @sozee = Sozee.new
+  end
+
+  def create
+    @sozee = Sozee.new(sozee_params)
+    if @sozee.save
+      redirect_to sozee_path(@sozee), notice: 'Your annonce was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sozee_params
+    params.require(:sozee).permit(:sozee_name, :description, :category, :price_per_hour, :user_id)
+  end
+end

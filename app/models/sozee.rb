c5fd1de1776af7f223deabd58c752131becc27a6
nathan reix <reixnathan@gmail.com>
2017-11-27 17:17:27 +0100

    index modifs

--- app/models/sozee.rb
+++ app/models/sozee.rb
@@ -1,2 +1,3 @@
 class Sozee < ApplicationRecord
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sozee.create(sozee_name: "Johnny", )
+Sozee.create(sozee_name: "Bruce Willis")
+Sozee.create(sozee_name: "Jean Francois Placé")

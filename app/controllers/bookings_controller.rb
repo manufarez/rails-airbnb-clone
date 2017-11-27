3013694ec0fc03f9302ceb8af0e841191ae5db88
Manuel Farez <manufarez@gmail.com>
2017-11-27 18:20:55 +0100

    Improvements on the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+    def create
+    @booking = Boooking.new(booking_params)
+    @sozee = Sozee.find(params[:sozee_id])
+    @booking.sozee = @sozee
+      if @booking.save
+        redirect_to sozee_path(@sozee)
+      else
+        render 'sozee/show'
+      end
+    end
+
+    def index
+    @booking = Booking.all
+    end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:sozee_id, :user_id, :location, :status)
+  end
 end

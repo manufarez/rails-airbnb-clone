dcf61e16325d99315cba9e87caa11013b6ccc6ad
Manuel Farez <manufarez@gmail.com>
2017-11-28 15:48:56 +0100

    conflict solving

--- app/models/sozee.rb
+++ app/models/sozee.rb
@@ -2,4 +2,7 @@ class Sozee < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  validates :sozee_name, uniqueness: true, presence: true
+  validates :category, presence: true
+  validates :price_per_hour, presence: true
 end

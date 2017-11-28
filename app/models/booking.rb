c6f3e69e88fe7a0ee11c3b4d4679dd21b4d85835
Manuel Farez <manufarez@gmail.com>
2017-11-28 15:33:50 +0100

    Validation rules set and tested for user and booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :sozee
+  validates :location, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  t.references
 end

da745a5e8a31b1084dc6354ac5b1600088e76c79
Manuel Farez <manufarez@gmail.com>
2017-11-29 14:22:40 +0100

    Master issue fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :bookings
+  has_many :sozees, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
